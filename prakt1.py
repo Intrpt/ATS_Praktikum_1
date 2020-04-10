a8f6d56619afca7db45780983820c99e95688eea
NightGun123 <acc.benutzen@gmail.com>
2020-04-10 17:27:53 +0200

    Entwicklung der buildModel funktion und fertigstellung diverser kleriner funktionen

--- prakt1.py
+++ prakt1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from Vector import Vector
 from Point import Point
 from Model import Model
+from Landmark import Landmark
 
 # --- MAIN ------
 if __name__ == '__main__':
@@ -31,6 +32,33 @@ def addVectors(v1, v2):
 #buildmodel(mid_x, mid_y, radius, LM1, LM2, LM3) return model
 def buildmodel(mid_x, mid_y, radius, LM1, LM2, LM3):
     m = Model(Point(mid_x,mid_y),radius)
+
+    # Wir wählen den ersten Landmark aus
+    LM_Working_on = LM1
+
+    # Muss 3 mal gemacht werden
+    # 2a) Wir berechnen die Entfernungen vom Snapshot mittelpunkt(=0,0) zum Mittelpunkt der Landmarks.
+    ent_model_zu_LM_Working_on = getDistance(m.mittelpunkt,LM_Working_on.mittelpunkt)
+    # Create fake LM
+    # 2b) Snapshotkreis radius auf die größe der berechneten Entfernungen vergrößern
+    LM_fake = Landmark(m.mittelpunkt,ent_model_zu_LM_Working_on)
+    # 2c) Die Schnittpunkte zwischen dem vergrößerten snapshot Radius und dem Landmark-kreis berechnet (Schnittpunkt1, Schnittpunkt2)
+    intersection1, intersection2 = getIntersection(LM_fake,LM_Working_on)
+    # 2d)
+    Schnittpunkt1 = Vector(intersection1)
+    Schnittpunkt2 = Vector(intersection2)
+    # 2e) Wir speichern die entfernung zwischen den beiden Schnittpunkten. (Als WInkel in grad)
+    ang_tmp = getAngBetween(Schnittpunkt1,Schnittpunkt2)
+    # 2f) Wir erstellen je einen Vektor von Snapshotkreis mittelpunkt zum Landmark mittelpunkt und speichern ihn.
+    tmp_p = Point(LM_Working_on.mittelpunkt.x - m.mittelpunkt.x, LM_Working_on.mittelpunkt.y - m.mittelpunkt.y)
+    vec_model_lm_mittelpunkt = Vector(tmp_p)
+
+    #3a)
+
+
+
+
+
     return m
     #TODO fertigstellen
 
@@ -45,24 +73,33 @@ def moveModel(model,mid_x, mid_y):
 #buildVt(Pair paare[]) return vektor
 def buildVt(paare):
     v = Vector(Point(0,0))
+    for value in paare:
+        v.p.x = v.p.x + normVector(value).p.x
+        v.p.y = v.p.y + normVector(value).p.y
     return v
 
 # Berechnet den Vp Vektor
 #buildVp(Pair paare[])  return vektor
 def buildVp(paare):
     v = Vector(Point(0,0))
+    for value in paare:
+        v.p.x = v.p.x + normVector(value).p.x
+        v.p.y = v.p.y + normVector(value).p.y
     return v
 
 # Berechnet den fertigen V vektor
 #buildV(Vp, Vt) return Vektor
 def buildV(Vp, Vt):
     v = Vector(Point(0,0))
+    v.p.x = Vp.p.x + 3 * Vt.p.x
+    v.p.y = Vp.p.y + 3 * Vt.p.y
     return v
 
 # Bereitet den fertigen Graphen vor
 #visualize(Vektor V)
 
 def visualize(V):
+    #TODO virtualisieren
     return 1
 
 
