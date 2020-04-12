404682ecaeaf6e6b6b6416bcb9e250bb6cf31cb1
NightGun123 <acc.benutzen@gmail.com>
2020-04-12 20:12:55 +0200

    Class Model ist now printable for a better overview. print(Model)

--- Model.py
+++ Model.py
@@ -21,8 +21,45 @@ class Model:
         self.vec_mid_G1 = self.vec_mid_G2 = self.vec_mid_G3 = None
 
     def __str__(self):
-        str_aufbau = "Das Model hat den Mittelpunkt bei: " +str(self.mittelpunkt.x)+"|"+str(self.mittelpunkt.y)
+        str_aufbau = "----\nDas Model hat den Mittelpunkt bei: " +str(self.mittelpunkt.x)+"|"+str(self.mittelpunkt.y)
+        str_aufbau = str_aufbau + "\nDer Radius ist: "+ str(self.radius)
+        str_aufbau = str_aufbau + "\nvec_mid_LM1: "+str(self.vec_mid_LM1.p.x)+"|"+str(self.vec_mid_LM1.p.y)
+        str_aufbau = str_aufbau + "\nvec_mid_LM2: "+str(self.vec_mid_LM2.p.x)+"|"+str(self.vec_mid_LM2.p.y)
+        str_aufbau = str_aufbau + "\nvec_mid_LM3: "+str(self.vec_mid_LM3.p.x)+"|"+str(self.vec_mid_LM3.p.y)
+        str_aufbau = str_aufbau + "\nvec_mid_G1: "+str(self.vec_mid_G1.p.x)+"|"+str(self.vec_mid_G1.p.y)
+        str_aufbau = str_aufbau + "\nvec_mid_G2: "+str(self.vec_mid_G2.p.x)+"|"+str(self.vec_mid_G2.p.y)
+        str_aufbau = str_aufbau + "\nvec_mid_G3: "+str(self.vec_mid_G3.p.x)+"|"+str(self.vec_mid_G3.p.y)
+
         str_aufbau = str_aufbau + "\n"
+        
+        str_aufbau = str_aufbau + "\nang_mid_LM1: "+str(self.ang_mid_LM1)+"°"
+        str_aufbau = str_aufbau + "\nang_mid_LM2: "+str(self.ang_mid_LM2)+"°"
+        str_aufbau = str_aufbau + "\nang_mid_LM3: "+str(self.ang_mid_LM3)+"°"
+        str_aufbau = str_aufbau + "\nang_mid_G1: "+str(self.ang_mid_G1)+"°"
+        str_aufbau = str_aufbau + "\nang_mid_G2: "+str(self.ang_mid_G2)+"°"
+        str_aufbau = str_aufbau + "\nang_mid_G3: "+str(self.ang_mid_G3)+"°"
+
+
+
+        str_aufbau = str_aufbau + "\n"
+
+
+        str_aufbau = str_aufbau + "\nsize_LM1: "+str(self.size_LM1)+"°"
+        str_aufbau = str_aufbau + "\nsize_LM2: "+str(self.size_LM2)+"°"
+        str_aufbau = str_aufbau + "\nsize_LM3: "+str(self.size_LM3)+"°"
+        str_aufbau = str_aufbau + "\nsize_G1: "+str(self.size_G1)+"°"
+        str_aufbau = str_aufbau + "\nsize_G2: "+str(self.size_G2)+"°"
+        str_aufbau = str_aufbau + "\nsize_G3: "+str(self.size_G3)+"°"
+
+        str_aufbau = str_aufbau + "\nang_1_LM1: "+str(self.ang_1_LM1)+"°"
+        str_aufbau = str_aufbau + "\nang_2_LM1: "+str(self.ang_2_LM1)+"°"
+        str_aufbau = str_aufbau + "\nang_1_LM2: "+str(self.ang_1_LM2)+"°"
+        str_aufbau = str_aufbau + "\nang_2_LM2: "+str(self.ang_2_LM2)+"°"
+        str_aufbau = str_aufbau + "\nang_1_LM3: "+str(self.ang_1_LM3)+"°"
+        str_aufbau = str_aufbau + "\nang_2_LM3: "+str(self.ang_2_LM3)+"°"
+
+
+        str_aufbau = str_aufbau + "\n----"
         return str_aufbau
 
         
