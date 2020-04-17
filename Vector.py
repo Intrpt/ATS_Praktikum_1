bc99a421e29a29824f6a0c9a5de8d1d8ac464362
Kai <Kai@fam-pietruska.de>
2020-04-17 04:42:03 -0700

    Added: main & Vector compare function

--- Vector.py
+++ Vector.py
@@ -3,3 +3,6 @@ class Vector:
     def __init__(self, p):
         #super(Vector, self).__init__()
         self.p = p
+
+    def __eq__(self, other):
+        return (self.p.x == other.p.x) && (self.p.y == other.p.y)
